5a86723fff58f331151d8b5c18a85c988b54af47
201078339 <philip_lamothe@hotmail.com>
2016-05-16 18:31:52 -0400

    PUSSY

--- src/ServiceQuestion.java
+++ src/ServiceQuestion.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.Socket;
+import java.sql.*;
+
 
 /**
  * Created by 201078339 on 2016-05-13.
@@ -14,9 +16,33 @@ public class ServiceQuestion implements Runnable{
     @Override
     public void run() {
         try{
+
+            String url = "jdbc:oracle:thin:@mercure.clg.qc.ca:1521:orcl";
+            String user = "L";
+            String pwd = "ORACLE1";
+            Connection CONN = null;
+
+            try {
+                DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+                System.out.println("Driver charger");
+            } catch (SQLException se) {
+                System.out.println(se.getMessage());
+            }
+            try {
+
+
+                CONN = DriverManager.getConnection(url, user, pwd);
+                System.out.println("Connecte");
+
+                PreparedStatement pst = null;
+            } catch (SQLException se) {
+                System.out.println(se.getMessage());
+            }
+
             reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
             writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
 
+            CallableStatement CalSup = CONN.prepareCall();
             writer.print("hello");
         }
         catch (IOException IOE){

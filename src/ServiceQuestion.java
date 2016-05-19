6ffd82dddf5a5983dde9fdd480f00f563794f26c
Dynacord <mathieuj-c@outlook.com>
2016-05-19 11:29:42 -0400

    Merge branch 'master' of C:\Users\201078339\Desktop\OrDragon with conflicts.

--- src/ServiceQuestion.java
+++ src/ServiceQuestion.java
@@ -17,13 +17,21 @@ public class ServiceQuestion implements Runnable{
     private BufferedReader reader;
     private PrintWriter writer;
 
+    private ChoiceDialog mDialogue;
+    private final String mReponse[] = {};
+    private List mListe;
+    private static final String mTitre = "Sélectionné la bonne réponse ???";
+
+
+
+
     ServiceQuestion(Socket soc){
         s = soc;
     }
 
     @Override
     public void run() {
-        try {
+        try{
 
             String url = "jdbc:oracle:thin:@mercure.clg.qc.ca:1521:orcl";
             String user = "L";
@@ -38,24 +46,36 @@ public class ServiceQuestion implements Runnable{
             }
             try {
 
-                try {
-                    CONN = DriverManager.getConnection(url, user, pwd);
-                    System.out.println("Connecte");
+            try {
+                CONN = DriverManager.getConnection(url, user, pwd);
+                System.out.println("Connecte");
 
-                    PreparedStatement pst = null;
-                } catch (SQLException se) {
-                    System.out.println(se.getMessage());
-                }
-
-                reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
-
-                //CallableStatement CalSup = CONN.prepareCall();
-                writer.print("hello");
-                writer.flush();
+                PreparedStatement pst = null;
+            } catch (SQLException se) {
+                System.out.println(se.getMessage());
             }
-            catch (IOException IOE) {
 
+            reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+
+            //CallableStatement CalSup = CONN.prepareCall();
+            writer.print("hello");
+            writer.flush();
+        }
+        catch (IOException IOE){
+
+               // ResultSet Allo = CalSup.executeQuery();
+
+                mDialogue = new ChoiceDialog(mListe.get(0),mListe);
+                mDialogue.setTitle(mTitre);
+                //mDialogue.setHeaderText(Allo.getString(1));
+
+                Optional reponse = mDialogue.showAndWait();
+                String choix = "Aucune réponse";
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } catch (Exception e) {
             e.printStackTrace();

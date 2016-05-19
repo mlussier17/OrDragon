0fb396655aeb8d5be0df4702d12489f81f3b2c59
Dynacord <mathieuj-c@outlook.com>
2016-05-19 14:40:29 -0400

    Get la Question

--- src/ServiceQuestion.java
+++ src/ServiceQuestion.java
@@ -1,3 +1,5 @@
+import oracle.jdbc.internal.OracleTypes;
+
 import java.io.*;
 import java.net.Socket;
 import java.sql.*;
@@ -25,21 +27,36 @@ public class ServiceQuestion implements Runnable{
             String id = reader.readLine();
             System.out.println(id);
 
-//            String url = "jdbc:oracle:thin:@mercure.clg.qc.ca:1521:orcl";
-//            Connection CONN = null;
+            String url = "jdbc:oracle:thin:@mercure.clg.qc.ca:1521:orcl";
+            Connection CONN = null;
 
-//            try {
-//                DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-//                System.out.println("Driver charger");
-//            } catch (SQLException se) {
-//                System.out.println(se.getMessage());
-//            }
-//            CONN = Database.getConnection();
+            try {
+                DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+                System.out.println("Driver charger");
+            } catch (SQLException se) {
+                System.out.println(se.getMessage());
+            }
+            CONN = Database.getConnection();
 
+            CallableStatement statementQuestion =CONN.prepareCall("{ ? = call QUESTIONSPKG.getQuestion(?)}");
+            statementQuestion.registerOutParameter(1, OracleTypes.CURSOR);
 
-            for (int i = 0; i < 5; ++i) {
-                writer.println("hello");
-                writer.flush();
+            statementQuestion.setInt(2,1);
+            statementQuestion.execute();
+
+            ResultSet rest=(ResultSet) statementQuestion.getObject(1);
+
+            for(int i=0;i<5;++i)
+            {
+                if(i==0) {
+                    writer.println(rest);
+                    writer.flush();
+                }
+                else
+                {
+                    writer.println();
+                    writer.flush();
+                }
             }
         }
         catch (IOException IOE) {
